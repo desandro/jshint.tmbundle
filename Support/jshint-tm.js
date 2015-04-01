edb1f989afae77d83f53cb854e2fdf7ef01ed610
David DeSandro <desandrocodes@gmail.com>
2015-04-01 10:21:15 -0400

    big refactor
    add error-item-template
    update JSHint to v2.6.3
    Add support for globals

--- Support/jshint-tm.js
+++ Support/jshint-tm.js
@@ -1,81 +1,91 @@
 
 var fs = require('fs');
 var path = require('path');
-var env = process.env || process.ENV;
+var env = process.env;
 var jshint = require('./jshint.js').JSHINT;
 
-var entities = {
-  '&': '&amp;',
-  '"': '&quot;',
-  '<': '&lt;',
-  '>': '&gt;'
-};
-
 var outputSrc = fs.readFileSync( __dirname + '/output.html', 'utf8');
-
-function html(s) {
-  return (s || '').replace(/[&"<>]/g, function(c) {return entities[c] || c;});
-}
-
-var existsSync = fs.existsSync || path.existsSync;
 
 function getRcFilePath() {
   var filePath = path.resolve( env.TM_FILEPATH, '../.jshintrc'  );
 
   // walk up directories, looking for .jshintrc
-  var exists = existsSync( filePath );
+  var exists = fs.existsSync( filePath );
   while ( !exists && filePath !== '/.jshintrc' ) {
     filePath = path.resolve( filePath, '../../.jshintrc' );
-    exists = existsSync( filePath );
+    exists = fs.existsSync( filePath );
   }
 
   return exists && filePath;
 }
 
-module.exports = function(options) {
-
-  var body = '';
-
-  var rcFilePath = getRcFilePath();
-
+function getOptions( rcFilePath ) {
+  var options;
   if ( rcFilePath ) {
-    body += '<div class="rc">Using options from ' + rcFilePath + '</div>';
+    // body += '<div class="rc">Using options from ' + rcFilePath + '</div>';
     var jshintrcFile = fs.readFileSync( rcFilePath, 'utf8' );
     options = JSON.parse( jshintrcFile );
   }
+  return options;
+}
+
+function extend( a, b ) {
+  for ( var prop in b ) {
+    a[ prop ] = b[ prop ];
+  }
+  return a;
+}
+
+module.exports = function() {
 
   var file = env.TM_FILEPATH;
-  var input = fs.readFileSync(file, 'utf8');
-
-  //remove shebang
+  var input = fs.readFileSync( file, 'utf8' );
+  // remove shebang
   input = input.replace(/^\#\!.*/, '');
 
-  if (!jshint(input, options)) {
-    jshint.errors.forEach(function(e, i) {
-      if (e) {
-        var link = 'txmt://open?url=file://' + encodeURI(file) + '&line=' + e.line + '&column=' + e.character;
-        body += ('<a class="txmt" href="' + link + '" id="e' + (i+1) + '">');
-        if (i < 9) {
-          body += '<b>'+(i+1)+'</b>';
-        }
-        body += e.reason;
-        if (e.evidence && !isNaN(e.character)) {
-          body += '<tt>';
-          body += html(e.evidence.substring(0, e.character-1));
-          body += '<em>';
-          body += (e.character <= e.evidence.length) ? html(e.evidence.substring(e.character-1, e.character)) : '&nbsp;';
-          body += '</em>';
-          body += html(e.evidence.substring(e.character));
-          body += '</tt>';
-        }
-        body += '</a>';
+  // get options from .jshintrc
+  var rcFilePath = getRcFilePath();
+  var options = getOptions( rcFilePath );
+  var globals = options && options.globals;
+
+  var errorsHTML = '';
+
+  if ( !jshint( input, options, globals ) ) {
+    jshint.errors.forEach( function( error, i ) {
+      if ( !error ) {
+        return;
       }
+      var context = extend( {}, error );
+      // link to position in TextMate
+      context.href = 'txmt://open?url=file://' + encodeURI( file ) +
+        '&line=' + error.line + '&column=' + error.character;
+      context.number = i + 1;
+
+      errorsHTML += getErrorItemHTML( context );
     });
   }
 
-  body = body || 'No JSHint errors :)';
+  errorsHTML = errorsHTML || 'No JSHint errors :)';
+  if ( options ) {
+    delete options.globals;
+  }
+  outputSrc = outputSrc.replace( '{{errors}}', errorsHTML )
+    .replace( '{{jshintrc}}', rcFilePath || 'no .jshintrc' )
+    .replace( '{{options}}', JSON.stringify( options ) )
+    .replace( '{{globals}}', JSON.stringify( globals ) );
 
-  console.log( outputSrc.replace( '{body}', body ) );
+  console.log( outputSrc );
   process.exit(205); //show_html
 
 };
+
+
+var errorItemTemplate = fs.readFileSync( __dirname + '/error-item-template.mustache', 'utf8' );
+var reMustache = /\{\{(([^\}\}])+)\}\}/gi;
+
+function getErrorItemHTML( context ) {
+  var html = errorItemTemplate.replace( reMustache, function( match, inner ) {
+    return context[ inner ];
+  });
+  return html;
+}
